2bfc77bfd915383da2f515fe43aca90f0423ad7a
marwamohamed51 <marwaamohamed51@gmail.com>
2024-04-24 23:31:47 +0300

    main

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('marwa/{id}', function ($id) {
-    return "welcome to my first website " .$id;
-});
 //id== is parameter
