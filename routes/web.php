1810b90f8f775908c52bed109ace8a51583df2af
marwamohamed51 <marwaamohamed51@gmail.com>
2024-04-24 23:20:23 +0300

    main

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('marwa', function () {
-    return "welcome to my website ";
+Route::get('marwa/{id}', function ($id) {
+    return "welcome to my website " .$id;
 });
